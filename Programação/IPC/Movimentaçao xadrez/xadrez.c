10ed4f5bc42ab927fbaccacef7c93fb761c1ee8d
Gabriel Teles <gabrielteles931@gmail.com>
2025-05-13 08:28:09 -0300

    Xadrez Desafio Novato
    Adicionando o print do movimento das peças individualmente.

---
+++ Programação/IPC/Movimentaçao xadrez/xadrez.c
@@ -0,0 +1,39 @@
+#include <stdio.h> 
+#include <string.h>
+#include <stdbool.h>
+
+int main() {
+    int movi,loop;
+
+    printf("Movimento da rainha: \n");
+    movi = 8;
+    loop = 1;
+    do
+    {
+        printf("Esquerda \n");   
+        loop = loop + 1;
+    } while (loop <= movi);
+    printf("\n");
+
+    printf("Movimento da torre: \n");
+    movi = 5;
+    loop = 1;
+    do
+    {
+        printf("Direita\n");
+        loop = loop + 1;  
+    } while (loop <= movi);
+    printf("\n");
+
+    printf("Movimento do bispo: \n");
+    movi = 5;
+    loop = 1;
+    do
+    {
+        printf("Direita\nCima \n");
+        loop = loop + 1;  
+    } while (loop <= movi);
+    printf("\n");
+
+    return 0;
+}
